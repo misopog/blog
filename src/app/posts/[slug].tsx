d6d7edd03516928e80c4b1fc9f46b3f2d38c5320
misopog <94299562+misopog@users.noreply.github.com>
2025-01-03 22:56:42 +0100

    mayb

--- src/app/posts/[slug].tsx
+++ src/app/posts/[slug].tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 import { Markdown } from '../../components/markdown'
 
 export async function generateStaticParams() {
-  const postsDirectory = path.join(process.cwd(), '/posts')
+  const postsDirectory = path.join(process.cwd(), './posts')
   const fileNames = fs.readdirSync(postsDirectory)
   
   return fileNames.map((fileName) => ({
