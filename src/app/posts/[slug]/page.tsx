c7517e8f38746c5f7c1c1d732321333a97db113b
misopog <majkenxdsvk@gmail.com>
2025-03-13 12:06:05 +0100

    fucking hell

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -5,11 +5,12 @@ import { notFound } from 'next/navigation'
 
 type Props = {
   params: Promise<{ slug: string }>
-  searchParams: { [key: string]: string | string[] | undefined }
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
-export default async function PostPage({ params }: Props) {
+export default async function PostPage({ params, searchParams }: Props) {
   const { slug } = await params
+  await searchParams // we dont use searchParams but need to await it
   const post = getPostBySlug(slug)
   const relatedPosts = getRelatedPosts(slug)
 
